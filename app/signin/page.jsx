404146cf79d06e4b22273403569466290074cd88
muhammedsubhan <muhammedsubhan692@gmail.com>
2024-07-08 05:41:58 +0500

    SignIn Page

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const SignUp = () => {
+const SignIn = () => {
   return (
     <>
       <div className="py-10 flex items-center justify-between lg:justify-center lg:gap-0 lg:pr-0 gap-16 pr-28 2xl:pr-10">
@@ -20,44 +20,12 @@ const SignUp = () => {
         <div className=" flex flex-col gap-10 w-[500px] xl:mr-10 lg:mr-0 sm:w-full sm:px-10">
           <div className="flex flex-col gap-6 ">
             <h1 className="text-3xl font-semibold lg:text-2xl">
-              Create an Account
+              Log in to Exclusive
             </h1>
             <p className="">Enter your details below.</p>
           </div>
           <div>
             <div className="flex flex-col gap-10 lg:gap-5">
-              <TextField
-                id="standard-basic"
-                label="Name"
-                variant="standard"
-                sx={{
-                  "& .MuiInput-underline:after": {
-                    borderBottomColor: "#7D8184", // Change border bottom color when focused
-                  },
-                  "& .MuiInputLabel-root": {
-                    color: "#7D8184", // Change label color when focused
-                  },
-                  "&:hover .MuiInput-underline:before": {
-                    borderBottomColor: "#7D8184", // Change border bottom color on hover
-                  },
-                }}
-              />
-              <TextField
-                id="standard-basic"
-                label="Username"
-                variant="standard"
-                sx={{
-                  "& .MuiInput-underline:after": {
-                    borderBottomColor: "#7D8184", // Change border bottom color when focused
-                  },
-                  "& .MuiInputLabel-root": {
-                    color: "#7D8184", // Change label color when focused
-                  },
-                  "&:hover .MuiInput-underline:before": {
-                    borderBottomColor: "#7D8184", // Change border bottom color on hover
-                  },
-                }}
-              />
               <TextField
                 id="standard-basic"
                 label="Email"
@@ -74,6 +42,7 @@ const SignUp = () => {
                   },
                 }}
               />
+
               <TextField
                 id="standard-basic"
                 label="Password"
@@ -94,7 +63,7 @@ const SignUp = () => {
           </div>
           <div className="flex flex-col gap-5">
             <button className=" bg-red-500  w-full h-[60px] rounded-md text-white font-medium">
-              Create Account
+              Login
             </button>
             <button className=" flex items-center justify-center gap-5 bg-white border border-gray-300  w-full h-[60px] rounded-md text-black font-medium">
               <Image
@@ -103,14 +72,15 @@ const SignUp = () => {
                 width={20}
                 alt="google-icon"
               />{" "}
-              Sign up with Google
+              Login with Google
             </button>
           </div>
-          <div className="text-center">
+          <div className="text-center flex flex-col gap-5">
+            <p className="font-medium text-red-500">Forget Password?</p>
             <p>
-              Already have account?{" "}
+              Don't have an Account?{" "}
               <span className="font-medium hover:underline underline-offset-4">
-                <Link href="/signin"> Log in</Link>
+                <Link href="/signup">Create</Link>
               </span>
             </p>
           </div>
@@ -120,4 +90,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default SignIn;
