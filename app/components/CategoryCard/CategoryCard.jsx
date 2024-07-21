ed0716308fe550794a924da2569c370c182ca80b
muhammedsubhan <muhammedsubhan692@gmail.com>
2024-07-21 07:56:53 +0500

    changes

--- app/components/CategoryCard/CategoryCard.jsx
+++ app/components/CategoryCard/CategoryCard.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const CategoryCard = () => {
   return (
     <>
-      <div className="border rounded-lg border-gray-400 w-[210px] py-8 mt-16 cursor-pointer 2xl:w-[180px] lg:w-[150px] lg:py-6 md:w-[120px] md:h-[140px] md:py-5">
+      <div className="border rounded-lg border-gray-400 w-[210px] py-8 mt-16 cursor-pointer 2xl:w-[180px] lg:w-[150px] lg:py-6 sm:w-[100px] sm:h-[140px] md:py-5">
         <div className="flex flex-col items-center justify-center gap-5">
           <Image src="/phone.png" width={60} height={60} alt="phones" />
           <p className="text-xl font-normal">Phones</p>
